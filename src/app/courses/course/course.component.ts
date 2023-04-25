46cd3fa5f78836fa3b749f0a4f6ebad0bc4a99f6
Sandeep Chopra <sandeepchopra7@gmail.com>
2023-04-25 18:39:01 -0400

    change the change detection strategy

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,4 +1,9 @@
-import { AfterViewInit, Component, OnInit } from "@angular/core";
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+} from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Course } from "../model/course";
 import { Observable, of } from "rxjs";
@@ -21,6 +26,7 @@ import { LessonEntityService } from "../services/lesson-entity.service";
   selector: "course",
   templateUrl: "./course.component.html",
   styleUrls: ["./course.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseComponent implements OnInit {
   course$: Observable<Course>;
